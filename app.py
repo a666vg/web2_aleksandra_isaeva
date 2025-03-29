06321a1145177f0c17a9541073aae0112f1dcacc
a666vg <abvgdedjzi@yandex.ru>
2025-03-29 12:01:43 +0300

    render /

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def about():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    # app.run(debug=True)
